8f612288eed611a0105907a94ee3630b10f566d9
Rudá Fraga Valle <rudavalle@gmail.com>
2016-03-12 19:53:39 -0300

    Update ExampleFor.cs

--- ExampleFor.cs
+++ ExampleFor.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +11,8 @@ namespace my_first_project.day1
 
         static void Main()
         {
+            //i++ short form for i = i + i
+            //for(initial value, condition, decrementor/incrementor)
             for(int i = 10; i>=0; i--)
             {
                 Console.WriteLine("i = " + i);
@@ -19,7 +21,7 @@ namespace my_first_project.day1
 
 
 
-        } 
+        }
 
 
 
