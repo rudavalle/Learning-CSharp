92dbf001258b08873d26107968cf7792aece0abf
Rudá Fraga Valle <rudavalle@gmail.com>
2016-03-12 19:58:07 -0300

    Example of While
    Simple example of while

---
+++ ExampleWhile.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_first_project.day1
+{
+    class ExampleWhile
+    {
+        static void Main()
+        {
+            int x = 10;
+
+            while (x >= 0)
+            {
+                Console.WriteLine(" x = " + x);
+                x--; //x = x - 1;
+            }
+
+
+
+        }
+
+
+    }
+}
