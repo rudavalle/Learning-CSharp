bcf6d7bbb6873a983e6d6fa0f15cc7883d31e30a
Rudá Fraga Valle <rudavalle@gmail.com>
2016-03-12 21:10:10 -0300

    Update ExerciseB.cs

--- ExerciseB.cs
+++ ExerciseB.cs
@@ -1,1 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace my_first_project.day1.exercises
+{
+    class ExerciseB
+    {
+        static void Main()
+        {
+
+            string[] strs = { "3,45", "3,87", "87,98", "56,7" };
+
+            double total = 0.0;
+
+            foreach (string temp in strs)
+            {
+                total += Convert.ToDouble(temp);
+
+            }
+
+
+            Console.WriteLine("Total = " + total);
+
+        }
+
+
+    }
+}
