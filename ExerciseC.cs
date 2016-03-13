c789f11c457ac293dd087a524c9cdc510315a5a1
Rudá Fraga Valle <rudavalle@gmail.com>
2016-03-12 21:15:07 -0300

    Review Exercise 3
    for a given string
    string a = "Hello World";
    
    print out "World".

---
+++ ExerciseC.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_first_project.day1.exercises
+{
+    class ExerciseC
+    {
+        static void Main()
+        {
+
+            string a = "Hello World";
+
+
+            Console.WriteLine(a.Substring(6));
+
+
+
+        }
+
+
+    }
+}
